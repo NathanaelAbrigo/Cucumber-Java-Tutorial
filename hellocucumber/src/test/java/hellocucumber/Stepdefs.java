62364bae342bf740f4d8df6532814da41330f7db
Nathanael Chris Abrigo <nathanael.abrigo@gmail.com>
2024-06-15 15:45:34 +0800

    changed to Scenario Outline format

--- hellocucumber/src/test/java/hellocucumber/Stepdefs.java
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -11,13 +11,40 @@ class IsItFriday {
     }
 }
 
+// public class Stepdefs {
+//     private String today;
+//     private String actualAnswer;
+
+//     @Given("today is Sunday")
+//     public void today_is_Sunday() {
+//         today = "Sunday";
+//     }
+
+//     @When("I ask whether it's Friday yet")
+//     public void i_ask_whether_it_s_Friday_yet() {
+//         actualAnswer = IsItFriday.isItFriday(today);
+//     }
+
+//     @Then("I should be told {string}")
+//     public void i_should_be_told(String expectedAnswer) {
+//         assertEquals(expectedAnswer, actualAnswer);
+//     }
+
+//     //add for 2nd scenario
+//     @Given("today is Friday")
+//     public void today_is_Friday() {
+//         today = "Friday";
+//     }
+// }
+
+//for Scenario Outline
 public class Stepdefs {
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("I ask whether it's Friday yet")
@@ -28,11 +55,5 @@ public class Stepdefs {
     @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
-    }
-
-    //add for 2nd scenario
-    @Given("today is Friday")
-    public void today_is_Friday() {
-        today = "Friday";
     }
 }
